512521b12a401531ba17e8515054053f564667dc
Yankuan Zhang <augustus.z@outlook.com>
2022-01-16 15:23:41 -0800

    style toast

--- HackMap/src/components/ToastMessage/index.tsx
+++ HackMap/src/components/ToastMessage/index.tsx
@@ -1,27 +1,7 @@
-import { Toast } from 'jimu-ui';
 import { React } from 'jimu-core';
+import { Toast, ToastType } from 'jimu-ui';
+import './index.css';
 
-
-export default class ToastMessage extends React.PureComponent {
-
-    constructor(props) {
-        super(props);
-
-        this.state = {
-
-        };
-      }
-
-
-    render() {
-        return (
-            <Toast 
-            type="INFO" 
-            open={!!this.props.toastText} 
-            text={this.props.toastText}>
-            </Toast>
-        )
-    }
-
-
-}
+export default ({ toastText }: { toastText: string }) => {
+  return <Toast type={ToastType.Info} open={!!toastText} text={toastText} className="hack-toast-message"></Toast>;
+};
