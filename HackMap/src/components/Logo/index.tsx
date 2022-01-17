ce919060690a3b1e23f0068bfdf9751b7458b4ee
Yankuan Zhang <augustus.z@outlook.com>
2022-01-16 18:08:28 -0800

    Update index.tsx

--- HackMap/src/components/Logo/index.tsx
+++ HackMap/src/components/Logo/index.tsx
@@ -2,41 +2,96 @@
 import { jsx } from 'jimu-core';
 
 export const Logo = () => (
-  <svg width="48" height="48" viewBox="0 0 48 48" fill="none" xmlns="http://www.w3.org/2000/svg">
-    <rect x="3.75708" y="4.27148" width="41.2286" height="41.2286" rx="8.04286" fill="#004874" stroke="white" />
-    <rect
-      x="7.25244"
-      y="14.2917"
-      width="16.8987"
-      height="33.7973"
-      rx="3.75526"
-      transform="rotate(-27.1163 7.25244 14.2917)"
-      fill="white"
-    />
-    <rect
-      x="16.7982"
-      y="17.8416"
-      width="3.14162"
-      height="7.51052"
-      rx="1.57081"
-      transform="rotate(-27.1163 16.7982 17.8416)"
-      fill="#004874"
-    />
-    <rect
-      x="19.1433"
-      y="27.8941"
-      width="8.131"
-      height="11.8863"
-      rx="1.88571"
-      transform="rotate(-27.1163 19.1433 27.8941)"
-      stroke="#004874"
-      stroke-width="1.25714"
-    />
-    <path
-      d="M30.0357 13.7965L30.0356 13.7973C29.997 15.5354 29.2472 17.1824 27.9579 18.3784C26.6692 19.5738 24.9467 20.2219 23.1691 20.1869L23.1677 20.1869C21.39 20.148 19.6958 19.4287 18.4581 18.1804C17.2197 16.9314 16.5394 15.2543 16.5739 13.5159L16.5739 13.5152C16.6389 10.4069 18.1424 7.75404 19.611 5.88664C21.0838 4.01397 22.5711 2.86769 22.7153 2.759L30.0357 13.7965ZM30.0357 13.7965C30.1006 10.6881 28.7088 7.97402 27.3193 6.04656M30.0357 13.7965L27.3193 6.04656M27.3193 6.04656C25.9294 4.11852 24.4947 2.91188 24.3475 2.79202M27.3193 6.04656L24.3475 2.79202M24.3475 2.79202C24.1183 2.60414 23.8323 2.50032 23.538 2.49417C23.2432 2.48801 22.9527 2.58011 22.7156 2.75883L24.3475 2.79202ZM25.1963 14.3092C25.3688 13.9655 25.464 13.5879 25.4732 13.2018L25.4733 13.1998C25.475 13.1161 25.511 13.032 25.5805 12.9675C25.6507 12.9024 25.7493 12.8635 25.8555 12.8657C25.9616 12.8679 26.0585 12.9109 26.1259 12.9789C26.1925 13.0461 26.225 13.1313 26.2234 13.2149C26.2144 13.596 26.1377 13.9716 25.9979 14.3259L25.1963 14.3092ZM24.0609 15.4715L25.9366 14.4707C25.8761 14.6049 25.8062 14.7355 25.7273 14.8617C25.4152 15.361 24.9717 15.7748 24.4389 16.0601C24.3501 16.1074 24.2427 16.1195 24.1421 16.0914C24.0418 16.0633 23.9633 15.9997 23.9188 15.9217C23.8748 15.8446 23.8652 15.7569 23.8894 15.6757C23.9137 15.594 23.9726 15.5186 24.0609 15.4715Z"
-      fill="#009AF2"
-      stroke="white"
-      stroke-width="1.26672"
-    />
+  <svg width="48" height="49" viewBox="0 0 48 49" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <path fill="#E5E5E5" d="M0 0h48v49H0z" />
+    <g filter="url(#a)">
+      <g clip-path="url(#b)">
+        <rect x="-16" y="-4" width="1440" height="1024" rx="16" fill="#fff" />
+        <g filter="url(#c)">
+          <path fill="#fff" d="M-16-4h1440v57H-16z" />
+          <rect x="3.8" y="4.8" width="41.2" height="41.2" rx="8" fill="#004874" stroke="#fff" />
+          <rect x="7.3" y="14.8" width="16.9" height="33.8" rx="3.8" transform="rotate(-27.1 7.3 14.8)" fill="#fff" />
+          <rect
+            x="16.8"
+            y="18.3"
+            width="3.1"
+            height="7.5"
+            rx="1.6"
+            transform="rotate(-27.1 16.8 18.3)"
+            fill="#004874"
+          />
+          <rect
+            x="19.1"
+            y="28.4"
+            width="8.1"
+            height="11.9"
+            rx="1.9"
+            transform="rotate(-27.1 19.1 28.4)"
+            stroke="#004874"
+            stroke-width="1.3"
+          />
+          <mask id="d" maskUnits="userSpaceOnUse" x="15.1" y="1.5" width="16.4" height="20.3" fill="#000">
+            <path fill="#fff" d="M15.1 1.5h16.4v20.3H15.1z" />
+            <path d="M23.2 20a6 6 0 0 0 4.3-1.6c1.2-1 1.9-2.6 1.9-4.1.1-5.8-5.2-10.3-5.5-10.5a.7.7 0 0 0-.8 0c-.2.1-5.8 4.4-5.9 10.2 0 1.6.6 3.1 1.7 4.2a6.2 6.2 0 0 0 4.3 1.9Zm.6-4.6.7-.7c.2-.3.3-.7.3-1 0-.3.2-.5.3-.7a1 1 0 0 1 1.5 0c.2.2.3.5.3.7 0 .7-.3 1.4-.6 2a4 4 0 0 1-1.6 1.4 1 1 0 0 1-.7.1 1 1 0 0 1-.6-.5 1 1 0 0 1-.1-.7c0-.3.2-.5.5-.6Z" />
+          </mask>
+          <path
+            d="M23.2 20a6 6 0 0 0 4.3-1.6c1.2-1 1.9-2.6 1.9-4.1.1-5.8-5.2-10.3-5.5-10.5a.7.7 0 0 0-.8 0c-.2.1-5.8 4.4-5.9 10.2 0 1.6.6 3.1 1.7 4.2a6.2 6.2 0 0 0 4.3 1.9Zm.6-4.6.7-.7c.2-.3.3-.7.3-1 0-.3.2-.5.3-.7a1 1 0 0 1 1.5 0c.2.2.3.5.3.7 0 .7-.3 1.4-.6 2a4 4 0 0 1-1.6 1.4 1 1 0 0 1-.7.1 1 1 0 0 1-.6-.5 1 1 0 0 1-.1-.7c0-.3.2-.5.5-.6Z"
+            fill="#009AF2"
+          />
+          <path
+            d="M23.2 20a6 6 0 0 0 4.3-1.6c1.2-1 1.9-2.6 1.9-4.1.1-5.8-5.2-10.3-5.5-10.5a.7.7 0 0 0-.8 0c-.2.1-5.8 4.4-5.9 10.2 0 1.6.6 3.1 1.7 4.2a6.2 6.2 0 0 0 4.3 1.9Zm.6-4.6.7-.7c.2-.3.3-.7.3-1 0-.3.2-.5.3-.7a1 1 0 0 1 1.5 0c.2.2.3.5.3.7 0 .7-.3 1.4-.6 2a4 4 0 0 1-1.6 1.4 1 1 0 0 1-.7.1 1 1 0 0 1-.6-.5 1 1 0 0 1-.1-.7c0-.3.2-.5.5-.6Z"
+            stroke="#fff"
+            stroke-width="2.5"
+            mask="url(#d)"
+          />
+        </g>
+      </g>
+    </g>
+    <defs>
+      <filter
+        id="a"
+        x="-32"
+        y="-14"
+        width="1472"
+        height="1056"
+        filterUnits="userSpaceOnUse"
+        color-interpolation-filters="sRGB"
+      >
+        <feFlood flood-opacity="0" result="BackgroundImageFix" />
+        <feColorMatrix in="SourceAlpha" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0" result="hardAlpha" />
+        <feMorphology radius="2" in="SourceAlpha" result="effect1_dropShadow_0_1" />
+        <feOffset dy="4" />
+        <feGaussianBlur stdDeviation="6" />
+        <feColorMatrix values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0.08 0" />
+        <feBlend in2="BackgroundImageFix" result="effect1_dropShadow_0_1" />
+        <feColorMatrix in="SourceAlpha" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0" result="hardAlpha" />
+        <feMorphology radius="4" in="SourceAlpha" result="effect2_dropShadow_0_1" />
+        <feOffset dy="6" />
+        <feGaussianBlur stdDeviation="10" />
+        <feColorMatrix values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0.1 0" />
+        <feBlend in2="effect1_dropShadow_0_1" result="effect2_dropShadow_0_1" />
+        <feBlend in="SourceGraphic" in2="effect2_dropShadow_0_1" result="shape" />
+      </filter>
+      <filter
+        id="c"
+        x="-16"
+        y="-4"
+        width="1440"
+        height="57"
+        filterUnits="userSpaceOnUse"
+        color-interpolation-filters="sRGB"
+      >
+        <feFlood flood-opacity="0" result="BackgroundImageFix" />
+        <feBlend in="SourceGraphic" in2="BackgroundImageFix" result="shape" />
+        <feColorMatrix in="SourceAlpha" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0" result="hardAlpha" />
+        <feOffset dy="-1" />
+        <feComposite in2="hardAlpha" operator="arithmetic" k2="-1" k3="1" />
+        <feColorMatrix values="0 0 0 0 0.792157 0 0 0 0 0.792157 0 0 0 0 0.792157 0 0 0 1 0" />
+        <feBlend in2="shape" result="effect1_innerShadow_0_1" />
+      </filter>
+      <clipPath id="b">
+        <rect x="-16" y="-4" width="1440" height="1024" rx="16" fill="#fff" />
+      </clipPath>
+    </defs>
   </svg>
 );
